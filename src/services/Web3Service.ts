957097135b9d55edc19dd7de12b86354b13f8502
devfelipenunes <devfelipenunes@gmail.com>
2024-05-17 15:55:10 -0300

    update

---
+++ src/services/Web3Service.ts
@@ -0,0 +1,14 @@
+import FactoryABI from "./abis/Factory.abi.json";
+import FightingABI from "./abis/Fighting.abi.json";
+import RobotsNFTABI from "./abis/RobotsNFT.abi.json";
+import RewardTokenABI from "./abis/RewardToken.abi.json";
+import RobotMarketABI from "./abis/RobotMarket.abi.json";
+import Growing from "./abis/Growing.abi.json";
+
+const FACTORY_ADDRESS = "0xd7bdD102F7E4b94D3eE6C89575a6fEf07847e87d";
+const FIGHTING_ADDRESS = "0x8b94Fff1c610327a589E7968d71215159a9a262A";
+const ROBOTSNFT_ADDRESS = "0xab19C6c63106F1AB6767C4376379a490c6d7C03f";
+const REWARDTOKEN_ADDRESS = "0x3E3066A43905a60c8a846829c8d521141b025a34";
+const ROBOTMARKET_ADDRESS = "0x2231AA1E8dbDcE245D9732Ac37Edf925ba2ce46F";
+const GROWING_ADDRESS = "0x79d9889CeE8F996bf3d18842e9F028C3D2f45D26";
+const CHAIN_ID = "0xYourChainId";
